f5a471d17dbf1e723deac076c77e2b0e0a7a8f25
Alexandre Defelicibus <adefelicibus@gmail.com>
2016-08-05 10:25:25 -0300

    Change image to use new pulsar

---
+++ pulsar/local_env.sh
@@ -0,0 +1,9 @@
+## Place local configuration variables used by Pulsar and run.sh in here. For example
+
+## If using the drmaa queue manager, you will need to set the DRMAA_LIBRARY_PATH variable,
+## you may also need to update LD_LIBRARY_PATH for underlying library as well.
+#export DRMAA_LIBRARY_PATH=/path/to/libdrmaa.so
+
+
+## If you wish to use a variety of Galaxy tools that depend on galaxy.eggs being defined,
+export GALAXY_HOME=/mnt/galaxy
